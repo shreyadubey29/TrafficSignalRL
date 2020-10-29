4af58ec61be4b778dde5d273b5bb37ccd0d91f1a
shreya <shreyadubeygs@gmail.com>
2020-10-29 15:42:32 -0600

    Modified code to use config_parameters.txt

--- Model.py
+++ Model.py
@@ -3,56 +3,69 @@
 Neural network model for getting action for current state
 """
 import numpy as np
-import keras
-from keras import layers, losses, optimizers
+import keras    
+from keras import layers, losses, optimizers, Input
 from keras.utils import plot_model
 import os
+from datetime import datetime
+import warnings
+warnings.filterwarnings("ignore")
 
 class TrainingModel:
-    def __init__(self):
-        
+    def __init__(self, input_dim, output_dim, batch_size, learning_rate):
+
         # hyperparameters
-        self.input_dim = 80
-        self.output_dim = 4
-        self.batch_size = 50
-        self.learning_rate = 0.01
-        
+        self.input_dim = input_dim
+        self.output_dim = output_dim
+        self.batch_size = batch_size
+        self.learning_rate = learning_rate
+
         # now build and compile the model
-        inputs = keras.Input(shape=(self.input_dim,))
-        
-        layer1 = layers.Dense(200, activation='relu')(inputs)
-        layer2 = layers.Dense(200,activation='relu')(layer1)
-        layer3 = layers.Dense(200,activation='relu')(layer2)
-        layer4 = layers.Dense(200,activation='relu')(layer3)
-        
-        outputs = layers.Dense(self.output_dim, activation='softmax')(layer4)
-        
-        model = keras.Model(inputs=inputs, output=outputs)
-        
+        inputs = Input(shape=(self.input_dim,))
+
+        layer1 = layers.Dense(200, activation="relu")(inputs)
+        layer2 = layers.Dense(200, activation="relu")(layer1)
+        layer3 = layers.Dense(200, activation="relu")(layer2)
+        layer4 = layers.Dense(200, activation="relu")(layer3)
+
+        outputs = layers.Dense(self.output_dim, activation="softmax")(layer4)
+
+        self.model = keras.Model(inputs=inputs, output=outputs)
+
         # using mean sqaured error loss and Adam optimizer
-        model.compile(loss=losses.mean_squared_error, optimizer=optimizers.Adam(lr=self.learning_rate))
-        
-        return model
-    
+        self.model.compile(
+            loss=losses.mean_squared_error,
+            optimizer=optimizers.Adam(lr=self.learning_rate),
+        )
+
     def predict_single(self, state):
-        
+
         # predict action value for a single state
-        state = np.reshape(state,[1, self.input_dim])
+        state = np.reshape(state, [1, self.input_dim])
         return self.model.predict(state)
-    
+
     def predict_batch(self, states):
-        
+
         # predict action values for a bacth of states
         return self.model.predict(states)
-        
+
     def train_model(self, input_states, target_q_s_a):
-        
+
         # train the model
-        self.model.fit(input_states, target_q_s_a, epochs=1, verbose=0)
-        
+        self.model.fit(input_states, target_q_s_a, epochs=10, verbose=0)
+
     def save_model(self, path):
         
+        timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
         # Save the current model
-        self.model.save(os.path.join(path,'trained_model.h5'))
-        plot_model(self.model, to_file=os.path.join(path, 'model.png'), show_shapes=True, show_layer_name=True)
-        
+        self.model.save(
+            os.path.join(
+                path, "trained_model_" + timestamp + ".h5"
+            )
+        )
+#        plot_model(
+#            self.model,
+#            to_file=os.path.join(path, "model_" + timestamp + ".png"),
+#            show_shapes=True,
+#            show_layer_names=True,
+#        )
--- Simulation.py
+++ Simulation.py
@@ -4,11 +4,12 @@ Code for running simulation for the traffic signal by the RL agent
 """
 import numpy as np
 import traci
-import os
-import Model, Memory
+import timeit
 import datetime
 import random
-import Traffic
+
+import warnings
+warnings.filterwarnings("ignore")
 
 """
     we only have 4 actions that the agent can take which are of turning the 
@@ -16,7 +17,7 @@ import Traffic
     decision and they will be the intemediate action that will happen if the
     traffic light changes from red to green or vice versa
 """
-# actions 
+# actions
 # action 0 - 00
 NS_GREEN = 0
 NS_YELLOW = 1
@@ -30,35 +31,52 @@ EW_YELLOW = 5
 EWL_GREEN = 6
 EWL_YELLOW = 7
 
+
 class Simulation:
-    
+
     # class variables
-    def __init__(self, model):
-        self.Model = model
-        self.Memory = Memory.Memory()
-        Traffic_gen = Traffic.TrafficGenerator()
-        self.gamma = 0.75
+    def __init__(
+        self,
+        Model,
+        Memory,
+        Traffic_gen,
+        sumo_cmd,
+        gamma,
+        max_steps,
+        green_duration,
+        yellow_duration,
+        num_states,
+        num_actions,
+        epochs,
+    ):
+        self.Model = Model
+        self.Memory = Memory
+        self.Traffic_gen = Traffic_gen
+        self.gamma = gamma
         self.step_count = 0
-        #self.sumo_cmd = sumo_cmd
-        self.max_steps = 5000
-        self.yellow_duration = 5
-        self.green_duration = 10
-        self.num_of_states = 80
-        self.num_of_actions = 4
+        self.sumo_cmd = sumo_cmd
+        self.max_steps = max_steps
+        self.yellow_duration = yellow_duration
+        self.green_duration = green_duration
+        self.num_of_states = num_states
+        self.num_of_actions = num_actions
         self.rewards_list = []
         self.cumulative_wait_time_list = []
         self.average_queue_length_list = []
-        self.epochs = 100
-           
+        self.epochs = epochs
+
     def run(self, episode, epsilon):
         """
         This function will run one episode and then it will start the training for the agent
         """
-        start_time = datetime.datetime.now()
+        #start_time = datetime.datetime.now()
+        start_time = timeit.default_timer()
         #start_time_str = start_time.strftime("%Y%m%d_%H%M%S")
-        
+
         # setup sumo
-        
+        self.Traffic_gen.create_route(episode)
+        traci.start(self.sumo_cmd)
+
         # initialize variables in start of episode
         self.step_count = 0
         self.waiting_times = {}
@@ -68,71 +86,72 @@ class Simulation:
         old_total_wait_time = 0
         old_state = -1
         old_action = -1
+
+        while self.step_count < self.max_steps:
         
-        while self.step_count <self.max_steps:
-            
             # get current state
             current_state = self.get_state()
-            
+
             # get reward for previous action
             current_total_wait = self.collect_waiting_times()
             reward = self.get_reward(old_total_wait_time, current_total_wait)
-            
+
             # saving data into memory
-            if self.step > 0:
-                self.Memory.add_sample(old_state, old_action, reward, current_state)
-                
+            if self.step_count > 0:
+                self.Memory.add_sample((old_state, old_action, reward, current_state))
+
             # choose action based on the current state
             action = self.choose_action(current_state, epsilon)
-            
+
             # check if the action chosen is different from the last action then activate yellow lights
             if self.step_count != 0 and action != old_action:
                 self.activate_yellow_lights(old_action)
                 self.simulate(self.yellow_duration)
-            
+
             # take the chosen action
             self.activate_green_lights(action)
             self.simulate(self.green_duration)
-            
+
             # updating the variables for next step
             old_state = current_state
             old_action = action
             old_total_wait_time = current_total_wait
-            
+
             self.episode_reward = self.episode_reward + reward
-            
+            self.step_count  = self.step_count + 1
+
         # save episode stats
         self.save_episode_stats()
-        print("Toatal reward:", reward, "-Epsilon: ", round(epsilon, 2))
+        print("Total reward:", reward, "| Epsilon: ", round(epsilon, 2))
         traci.close()
-        simulation_time = round(datetime.datetime.now() - start_time, 1)
-        
+        simulation_time = round(timeit.default_timer() - start_time, 1)
+
         # now start training
-        train_start_time = datetime.datetime.now()
+        train_start_time = timeit.default_timer()
         for e in range(self.epochs):
             self.replay()
-        
-        training_time = round(datetime.datetime.now() - train_start_time, 1)
-        
+
+        training_time = round(timeit.default_timer() - train_start_time, 1)
+
         return simulation_time, training_time
-                
+
     def get_state(self):
         state = np.zeros(self.num_of_states)
-        
+
         car_list = traci.vehicle.getIDList()
-        
+
         for car_id in car_list:
             lane_position = traci.vehicle.getLanePosition(car_id)
             lane_id = traci.vehicle.getLaneID(car_id)
             # manipulating the value so that the nearest car near the traffic light has position 0
             lane_position = 750 - lane_position
-            
+
             if lane_position < 8:
                 cell_no = 0
             elif lane_position < 16:
                 cell_no = 1
             elif lane_position < 32:
-                cell_no = 2 
+                cell_no = 2
             elif lane_position < 64:
                 cell_no = 3
             elif lane_position < 128:
@@ -147,7 +166,7 @@ class Simulation:
                 cell_no = 8
             elif lane_position < 750:
                 cell_no = 9
-            
+
             # finding cell prefix from lane id
             if lane_id == "W2TL_0":
                 cell_prefix = 0
@@ -167,8 +186,8 @@ class Simulation:
                 cell_prefix = 7
             else:
                 cell_prefix = -1
-                
-            if cell_prefix >=1 and cell_prefix <=7:
+
+            if cell_prefix >= 1 and cell_prefix <= 7:
                 car_cell = int(str(cell_prefix) + str(cell_no))
                 valid_car = True
             elif cell_prefix == 0:
@@ -177,12 +196,12 @@ class Simulation:
             else:
                 # cars that are crossing or have crossed the intersection are not valid
                 valid_car = False
-                
+
             if valid_car:
                 state[car_cell] = 1
-            
+
         return state
-        
+
     def get_reward(self, old_wait_time, current_wait_time):
         reward = 0
         reward = old_wait_time - current_wait_time
@@ -196,21 +215,21 @@ class Simulation:
             return random.randint(0, self.num_of_actions - 1)
         else:
             return np.argmax(self.Model.predict_single(state))
-        
+
     def activate_yellow_lights(self, action):
         yellow_code = action * 2 + 1
         traci.trafficlight.setPhase("TL", yellow_code)
-        
+
     def activate_green_lights(self, action):
-        if action==0:
+        if action == 0:
             traci.trafficlight.setPhase("TL", NS_GREEN)
-        elif action==1:
+        elif action == 1:
             traci.trafficlight.setPhase("TL", NSL_GREEN)
-        elif action==2:
+        elif action == 2:
             traci.trafficlight.setPhase("TL", EW_GREEN)
-        elif action==3:
+        elif action == 3:
             traci.trafficlight.setPhase("TL", EWL_GREEN)
-            
+
     def get_queue_length(self):
         """
         Calculate the toatl number of cars at speed = 0 in each incoming lane
@@ -219,22 +238,22 @@ class Simulation:
         S_lane = traci.edge.getLastStepHaltingNumber("S2TL")
         E_lane = traci.edge.getLastStepHaltingNumber("E2TL")
         W_lane = traci.edge.getLastStepHaltingNumber("W2TL")
-        
+
         total_queue_length = N_lane + S_lane + W_lane + E_lane
         return total_queue_length
-        
+
     def save_episode_stats(self):
         self.rewards_list.append(self.episode_reward)
         self.cumulative_wait_time_list.append(self.sum_waiting_time)
         self.average_queue_length_list.append(self.sum_queue_length)
-        
+
     def simulate(self, steps_todo):
         """
         Perform steps in sumo
         """
-        if(self.step_count + steps_todo) >= self.max_steps:
+        if (self.step_count + steps_todo) >= self.max_steps:
             steps_todo = self.max_steps - self.step_count
-            
+
         while steps_todo > 0:
             traci.simulationStep()
             self.step_count += 1
@@ -242,14 +261,14 @@ class Simulation:
             queue_length = self.get_queue_length()
             self.sum_queue_length += queue_length
             self.sum_waiting_time += queue_length
-            
+
     def collect_waiting_times(self):
         """
         Get waiting time for every car on the incoming roads
         """
         incoming_roads = ["E2TL", "N2TL", "W2TL", "S2TL"]
         car_list = traci.vehicle.getIDList()
-        
+
         for car_id in car_list:
             wait_time = traci.vehicle.getAccumulatedWaitingTime(car_id)
             road_id = traci.vehicle.getRoadID(car_id)
@@ -258,28 +277,28 @@ class Simulation:
             else:
                 if car_id in self.waiting_times:
                     del self.waiting_times[car_id]
-        
+
         total_waiting_time = sum(self.waiting_times.values())
         return total_waiting_time
-                    
+
     def replay(self):
         """
         Get samples from memory and then use them to update the learning equation and then train
         """
-        batch = self.Memory.get_sample(self.Model.batch_size)
-        
+        batch = self.Memory.get_samples(self.Model.batch_size)
+
         if len(batch) > 0:
             states = np.array([val[0] for val in batch])
             next_states = np.array([val[3] for val in batch])
-            
+
             # prediction
             current_qsa_value = self.Model.predict_batch(states)
             next_qsa_value = self.Model.predict_batch(next_states)
-            
+
             # set x and y arrays for training
             x = np.zeros((len(batch), self.num_of_states))
             y = np.zeros((len(batch), self.num_of_actions))
-            
+
             n = 0
             for b in batch:
                 state, action, reward, next_state = b[0], b[1], b[2], b[3]
@@ -287,7 +306,6 @@ class Simulation:
                 current_q[action] = reward + self.gamma * np.amax(next_qsa_value[n])
                 x[n] = state
                 y[n] = current_q
-                n +=1
-            
+                n += 1
+
             self.Model.train_model(x, y)
-            
